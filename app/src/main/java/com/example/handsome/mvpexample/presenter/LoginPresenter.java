7d360cbb5244dd8939d64916db4ec560b7d74cb6
HSTsou <aids71214@gmail.com>
2017-01-04 22:48:54 +0800

    unit test modified

--- app/src/main/java/com/example/handsome/mvpexample/presenter/LoginPresenter.java
+++ app/src/main/java/com/example/handsome/mvpexample/presenter/LoginPresenter.java
@@ -1,8 +1,5 @@
 package com.example.handsome.mvpexample.presenter;
 
-import android.os.Handler;
-import android.os.Looper;
-
 import com.example.handsome.mvpexample.model.IUser;
 import com.example.handsome.mvpexample.model.UserModel;
 import com.example.handsome.mvpexample.view.ILoginView;
@@ -14,16 +11,17 @@ import com.example.handsome.mvpexample.view.ILoginView;
 public class LoginPresenter implements ILoginPresenter {
     ILoginView iLoginView;
     IUser user;
-    Handler handler;
+
+    private static final String TEST_NAME = "mvp";
+    private static final String TEST_PASSWD = "mvp";
 
     public LoginPresenter(ILoginView iLoginView) {
         this.iLoginView = iLoginView;
         initUser();
-        handler = new Handler(Looper.getMainLooper());
     }
 
     private void initUser() {
-        user = new UserModel("mvp","mvp");
+        user = new UserModel(TEST_NAME, TEST_PASSWD);
     }
 
     @Override
@@ -35,16 +33,11 @@ public class LoginPresenter implements ILoginPresenter {
     public void doLogin(String name, String passwd) {
         Boolean isLoginSuccess = true;
         final int code = user.checkUserValidity(name, passwd);// 1 = true, means correct name and pw.
-        if (code!=1) {
+        if (code != 1) {
             isLoginSuccess = false;
         }
         final Boolean result = isLoginSuccess;
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                iLoginView.onLoginResult(result, code);
-            }
-        }, 3000);
+        iLoginView.onLoginResult(result, code);
     }
 
     @Override
--- app/src/test/java/com/example/handsome/mvpexample/presenter/LoginPresenterTest.java
+++ app/src/test/java/com/example/handsome/mvpexample/presenter/LoginPresenterTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static org.mockito.Mockito.mock;
+
 /**
  * Created by handsome on 2017/1/4.
  */
@@ -13,27 +15,23 @@ public class LoginPresenterTest {
 
     @Mock
     ILoginView iLoginView;
+    @Mock
     LoginPresenter loginPresenter;
 
     @Before
     public void setup() throws Exception {
-        loginPresenter = new LoginPresenter(iLoginView);
+       loginPresenter = mock(LoginPresenter.class);
 
     }
 
     @Test
-    public void clear() throws Exception {
+    public void doLoginTest() throws Exception {
+        String name ="mvp";
+        String passwd = "mvp";
 
-    }
-
-    @Test
-    public void doLogin() throws Exception {
-
-    }
-
-    @Test
-    public void setProgressBarVisibility() throws Exception {
-
+       /* when(loginPresenter.doLogin(name, passwd)).thenReturn(1);
+        int abs = mathUtils.abs(-1);
+        Assert.assertEquals(abs, 1);*/
     }
 
 }
